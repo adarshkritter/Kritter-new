e32662bd0302cb41d59a20c1df7d118994459f2c
Masss12345 <sureshadarsh516@gmail.com>
2021-12-20 19:41:12 +0530

    New changes.. added mysql

--- src/com/company/Trial.java
+++ src/com/company/Trial.java
@@ -1,9 +1,10 @@
 package com.company;
 
 import java.io.*;
-import java.io.File;  // Import the File class
-import java.io.FileWriter;
-import java.io.IOException;  // Import the IOException class to handle errors
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Trial {
     void createFile() {
@@ -23,7 +24,8 @@ public class Trial {
             e.printStackTrace();
         }
     }
-    void readFile(){
+
+    void readFile() {
         File myObj = new File("abc.txt");
         if (myObj.exists()) {
             System.out.println("File name: " + myObj.getName());
@@ -35,17 +37,65 @@ public class Trial {
             System.out.println("The file does not exist.");
         }
     }
-    public static void main(String[] args) throws Exception{
-        //Trial e1 =new Trial();
-        //e1.createFile();
-        FileReader fr=new FileReader("adarsh.txt");
-        BufferedReader br=new BufferedReader(fr);
+    void showFile() throws Exception {
+        FileReader fr = new FileReader("adarsh.txt");
+        BufferedReader br = new BufferedReader(fr);
 
         int i;
-        while((i=br.read())!=-1){
-            System.out.print((char)i);
+        while ((i = br.read()) != -1) {
+            System.out.print((char) i);
         }
         br.close();
         fr.close();
+    }
+    void mysqlReader() throws SQLException {
+        String MySQLURL = "jdbc:mysql://localhost:3306/kritter?useSSL=false";
+        String databseUserName = "root";
+        String databasePassword = "Ad@8129405010";
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(MySQLURL, databseUserName, databasePassword);
+            if (con != null) {
+                System.out.println("Database connection is successful !!!!");
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        con.close();
+    }
+
+    void addValuestoMysql() throws SQLException {
+        String MySQLURL = "jdbc:mysql://localhost:3306/kritter?useSSL=false";
+        String databseUserName = "root";
+        String databasePassword = "Ad@8129405010";
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(MySQLURL, databseUserName, databasePassword);
+            if (con != null) {
+                String sql = "INSERT INTO bank VALUES (101, 'Adarsh Suresh', 22, 'M', 8129405010)";
+
+                PreparedStatement statement = con.prepareStatement(sql);
+                /*
+                statement.setInt(1, 101);
+                statement.setString(2, "Adarsh Suresh");
+                statement.setInt(3, 22);
+                statement.setString(4, "M");
+                statement.setInt(5, 8129405010);
+*/
+                int rowsInserted = statement.executeUpdate();
+                if (rowsInserted > 0) {
+                    System.out.println("A new user was inserted successfully!");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Trial e1 =new Trial();
+        e1.mysqlReader();
+        e1.addValuestoMysql();
+
+    }
+}
