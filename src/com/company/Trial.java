b751210e9f9307cc40ec3bc99f49ab193d4e211c
Masss12345 <sureshadarsh516@gmail.com>
2021-12-23 11:41:26 +0530

    with sql code

--- src/com/company/Trial.java
+++ src/com/company/Trial.java
@@ -1,10 +1,7 @@
 package com.company;
 
 import java.io.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Trial {
     void createFile() {
@@ -37,6 +34,7 @@ public class Trial {
             System.out.println("The file does not exist.");
         }
     }
+
     void showFile() throws Exception {
         FileReader fr = new FileReader("adarsh.txt");
         BufferedReader br = new BufferedReader(fr);
@@ -48,7 +46,8 @@ public class Trial {
         br.close();
         fr.close();
     }
-    void mysqlReader() throws SQLException {
+
+    void mysqlReader() {
         String MySQLURL = "jdbc:mysql://localhost:3306/kritter?useSSL=false";
         String databseUserName = "root";
         String databasePassword = "Ad@8129405010";
@@ -60,8 +59,13 @@ public class Trial {
             }
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
-        con.close();
     }
 
     void addValuestoMysql() throws SQLException {
@@ -72,7 +76,7 @@ public class Trial {
         try {
             con = DriverManager.getConnection(MySQLURL, databseUserName, databasePassword);
             if (con != null) {
-                String sql = "INSERT INTO bank VALUES (101, 'Adarsh Suresh', 22, 'M', 8129405010)";
+                String sql = "INSERT INTO bank VALUES (101, 'Adarsh Suresh', 22, 'M', '8129405010')";
 
                 PreparedStatement statement = con.prepareStatement(sql);
                 /*
@@ -92,10 +96,59 @@ public class Trial {
         }
     }
 
+    void updateMysql() throws SQLException {
+        String MySQLURL = "jdbc:mysql://localhost:3306/kritter?useSSL=false";
+        String databseUserName = "root";
+        String databasePassword = "Ad@8129405010";
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(MySQLURL, databseUserName, databasePassword);
+            if (con != null) {
+                String sql = "update customer set balance=? where accountNo=?";
+
+                PreparedStatement statement = con.prepareStatement(sql);
+                statement.setFloat(1, 5000);
+                statement.setFloat(2, 87915);
+                /*    statement.setFloat(3, tempAc);
+                 */
+                int rowsInserted = statement.executeUpdate();
+                if (rowsInserted > 0) {
+                    System.out.println("Mission successful!");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void selectMysql() throws SQLException {
+        String MySQLURL = "jdbc:mysql://localhost:3306/kritter?useSSL=false";
+        String databseUserName = "root";
+        String databasePassword = "Ad@8129405010";
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(MySQLURL, databseUserName, databasePassword);
+            if (con != null) {
+                System.out.println("Database connection is successful !!!!");
+                Statement stmt=con.createStatement();
+                ResultSet rs=stmt.executeQuery("select * from customer");
+                while(rs.next())
+                    System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+                con.close();
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
     public static void main(String[] args) throws Exception {
-        Trial e1 =new Trial();
-        e1.mysqlReader();
-        e1.addValuestoMysql();
+        Trial e1 = new Trial();
+        //e1.mysqlReader();
+        //e1.updateMysql();
+        e1.selectMysql();
 
     }
 }
